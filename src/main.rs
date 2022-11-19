75562f690d5872acae89ce390872ddc836252e68
DavidJArnold <david.14587@gmail.com>
2022-11-19 13:01:18 +1100

    Read words from text file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod word_tree;
 use crate::word_tree::WordTree;
+use std::fs;
 
 fn main() {
-    let words = vec!["abc", "acc", "abb", "bad", "a", "dab"];
+    let file_path = "src/words.txt";
+
+    let contents = fs::read_to_string(file_path).expect("");
+    let words = contents.lines().collect::<Vec<&str>>();
+
     let arena = WordTree::new(words);
 
     println!("Tree created!");
