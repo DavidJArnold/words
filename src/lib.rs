21fa1ecd6e060d8367c4f504a33934629f9dcc5a
DavidJArnold <david.14587@gmail.com>
2022-12-03 12:36:55 +1100

    Finished word search

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,8 @@ mod tests {
         use crate::word_tree::WordTree;
         let words = vec!["abc", "acc", "abb", "bad", "a", "dab"];
         let mut arena = WordTree::new(words);
-        let test_node = arena.find_node(0, 'a')[0].id;
-        assert!(arena.nodes[test_node].id.id == 0 as usize);
+        let test_node = arena.find_node(0, 'a')[0];
+        assert!(arena.nodes[test_node].id == 0 as usize);
         let node_ids = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
         let depths = vec![0, 1, 2, 1, 2, 2, 0, 1, 2, 0, 1, 2];
         let data = vec!['a', 'b', 'c', 'c', 'c', 'b', 'b', 'a', 'd', 'd', 'a', 'b'];
@@ -31,7 +31,7 @@ mod tests {
             assert!(node.terminal == terminal[idx]);
             match node.parent {
                 Some(node_id) => {
-                    assert_eq!(node_id.id as i32, parent[idx])
+                    assert_eq!(node_id as i32, parent[idx])
                 }
                 None => {
                     assert!(parent[idx] == -1)
--- src/word_tree.rs
+++ src/word_tree.rs
@@ -5,17 +5,12 @@ pub struct WordTree {
     pub nodes: Vec<Node>,
 }
 
-#[derive(Debug, PartialEq, Copy, Clone, Serialize, Deserialize)]
-pub struct NodeId {
-    pub id: usize,
-}
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Node {
     pub data: char,
-    pub id: NodeId,
+    pub id: usize,
     pub depth: i32,
-    pub parent: Option<NodeId>,
+    pub parent: Option<usize>,
     pub terminal: bool,
 }
 
@@ -26,9 +21,9 @@ impl WordTree {
         return arena;
     }
 
-    pub fn find_node(&mut self, depth: i32, data: char) -> Vec<NodeId> {
+    pub fn find_node(&mut self, depth: i32, data: char) -> Vec<usize> {
         // find node by data
-        let mut matching_nodes: Vec<NodeId> = vec![];
+        let mut matching_nodes: Vec<usize> = vec![];
         for nodes in &mut self.nodes {
             if nodes.data == data && nodes.depth == depth {
                 matching_nodes.push(nodes.id);
@@ -37,7 +32,7 @@ impl WordTree {
         return matching_nodes;
     }
 
-    fn find_node_parent(&mut self, depth: i32, data: char, parent: &mut NodeId) -> Option<NodeId> {
+    fn find_node_parent(&mut self, depth: i32, data: char, parent: &usize) -> Option<usize> {
         // find node by data
         for nodes in &mut self.nodes {
             if nodes.data == data && nodes.depth == depth && depth > 0 {
@@ -64,11 +59,10 @@ impl WordTree {
         &mut self,
         data: char,
         depth: i32,
-        parent: Option<NodeId>,
+        parent: Option<usize>,
         terminal: bool,
     ) -> usize {
-        let index = self.nodes.len();
-        let id = NodeId { id: index };
+        let id = self.nodes.len();
 
         let node = Node {
             data,
@@ -80,28 +74,28 @@ impl WordTree {
 
         self.nodes.push(node);
 
-        return index;
+        return id;
     }
 
     pub fn get_node(&self, id: usize) -> Option<Node> {
         for node in &self.nodes {
-            if node.id.id == id {
+            if node.id == id {
                 return Some(node.to_owned());
             };
         }
         return None;
     }
 
-    fn make_terminal(&mut self, node_id: NodeId) {
-        let node = &mut self.nodes[node_id.id];
+    fn make_terminal(&mut self, node_id: usize) {
+        let node = &mut self.nodes[node_id];
         node.terminal = true;
     }
 
-    fn get_children(&self, id: usize) -> Vec<NodeId> {
-        let mut children: Vec<NodeId> = vec![];
+    fn get_children(&self, id: usize) -> Vec<usize> {
+        let mut children: Vec<usize> = vec![];
         for node in &self.nodes {
             if node.depth > 0 {
-                if node.parent.unwrap().id == id {
+                if node.parent.unwrap() == id {
                     children.push(node.id);
                 }
             }
@@ -110,16 +104,16 @@ impl WordTree {
     }
 
     pub fn remove_node(&mut self, id: usize) {
-        let children = self.get_children(self.get_node(id).unwrap().id.id);
-        let node_idx = self.nodes.iter().position(|x| x.id.id == id).unwrap();
+        let children = self.get_children(self.get_node(id).unwrap().id);
+        let node_idx = self.nodes.iter().position(|x| x.id == id).unwrap();
         self.nodes.remove(node_idx);
         for child in children {
-            self.remove_node(child.id);
+            self.remove_node(child);
         }
     }
 
-    fn nodes_at_depth(&self, depth: i32) -> Vec<NodeId> {
-        let mut nodes: Vec<NodeId> = vec![];
+    pub fn nodes_at_depth(&self, depth: i32) -> Vec<usize> {
+        let mut nodes: Vec<usize> = vec![];
         for node in &self.nodes {
             if node.depth == depth {
                 nodes.push(node.id);
@@ -138,17 +132,17 @@ impl WordTree {
         return max_depth;
     }
 
-    fn print_children(&self, node: NodeId, level: i32) {
-        let kids = self.get_children(node.id);
+    fn print_children(&self, node_id: usize, level: i32) {
+        let kids = self.get_children(node_id);
         if kids.len() > 0 {
             for child in kids {
                 // println!("{:?}", self.get_node(child.id).unwrap());
                 print!(
                     "{}{}",
                     "|".repeat((level).try_into().unwrap()),
-                    self.get_node(child.id).unwrap().data
+                    self.get_node(child).unwrap().data
                 );
-                if self.get_node(child.id).unwrap().terminal {
+                if self.get_node(child).unwrap().terminal {
                     println!("*");
                 } else {
                     println!("");
@@ -158,69 +152,82 @@ impl WordTree {
         }
     }
 
+    pub fn get_words(&self, length: i32) -> Vec<String> {
+        // if length is None, get words of all length, otherwise just words of particular length
+        let node_list = self.nodes_at_depth(length-1);
+        let mut words: Vec<String> = vec![];
+        for node in node_list {
+            let mut current_word: String = String::from("");
+            if self.get_node(node).unwrap().terminal {
+                let mut current_node = self.get_node(node).unwrap();
+                for idx in 0..length {
+                    current_word.push(current_node.data);
+                    if idx + 1 < length {
+                        current_node = match current_node.parent {
+                            Some(node_id) => self.get_node(node_id).unwrap(),
+                            None => break
+                        };
+                    }
+                    // current_node = self.get_node(.id).unwrap();
+                };
+            }
+            if current_word.len() as i32 == length {
+                words.push(current_word);
+            }
+        };
+        let mut rev_words: Vec<String> = vec![];
+        for word in &words {
+            rev_words.push(word.chars().rev().collect::<String>());
+        };
+        return rev_words
+    }
+
     pub fn dbg(&self) {
         for node in &self.nodes {
             println!("{:?}", node);
         }
     }
 
-    pub fn disp(&self) {
+    pub fn disp(&self) -> &Self {
         let root_nodes = self.nodes_at_depth(0);
 
         for node in root_nodes {
-            // println!("{:?}", self.get_node(node.id).unwrap());
-            if self.get_node(node.id).unwrap().terminal {
-                println!("{}*", self.get_node(node.id).unwrap().data);
+            if self.get_node(node).unwrap().terminal {
+                println!("{}*", self.get_node(node).unwrap().data);
             } else {
-                println!("{}", self.get_node(node.id).unwrap().data);
+                println!("{}", self.get_node(node).unwrap().data);
             }
 
             let level: i32 = 1;
             self.print_children(node, level);
         }
+        return self
     }
 
     fn build_word_tree(&mut self, words: Vec<&str>) {
         for word in words {
-            // println!("{}", word);
-
             let mut word_iterator = word.chars();
             let ltr = word_iterator.next().unwrap();
-
-            // println!("First letter {}", ltr);
-            // first node
             let matched_nodes = self.find_node(0, ltr);
             
             let first_node: usize = if matched_nodes.len() != 0 {
-                // println!("Re-use a node");
-                matched_nodes[0].id
+                matched_nodes[0]
             } else {
-                // println!("Create a new node");
                 let new_node: usize = self.add_node(ltr, 0, None, false);
-                self.get_node(new_node).unwrap().id.id
+                self.get_node(new_node).unwrap().id
             };
-            // println!("Just created {}", first_node);
-            // println!("Now the arena looks like: {:?}", self);
-            // thereafter...
-            let mut prev_node_id: NodeId = self.get_node(first_node).unwrap().id;
+            let mut prev_node_id: usize = self.get_node(first_node).unwrap().id;
 
             for (idx, ltr) in word_iterator.enumerate() {
-                // println!("next letter {} at depth {}", ltr, idx as i32 + 1);
                 let next_node = match self.find_node_parent(idx as i32 + 1, ltr, &mut prev_node_id)
                 {
-                    Some(node_id) => node_id.id,
+                    Some(node_id) => node_id,
                     None => self.add_node(ltr, idx as i32 + 1, Some(prev_node_id), false),
                 };
-                // println!("Just created {}", next_node);
-                // println!("Now the arena looks like: {:?}", self);
-                // println!("Get the NodeId {:?}", self.get_node(next_node));
                 prev_node_id = self.get_node(next_node).unwrap().id
             }
-            // println!("Finished word, set last node to terminal");
-            let prev_node = self.get_node(prev_node_id.id).unwrap();
-            // println!("{:?}", prev_node);
+            let prev_node = self.get_node(prev_node_id).unwrap();
             self.make_terminal(prev_node.id);
-            // println!("{:?}", prev_node);
         }
     }
 
